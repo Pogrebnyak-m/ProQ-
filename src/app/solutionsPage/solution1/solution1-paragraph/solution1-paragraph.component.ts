330630e8b3a2ab7c16e528935aee72eac03a11c7
Maria <pogrebnyak-2002@bk.ru>
2021-07-13 06:01:35 +0300

    11th commit

---
+++ src/app/solutionsPage/solution1/solution1-paragraph/solution1-paragraph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solution1-paragraph',
+  templateUrl: './solution1-paragraph.component.html',
+  styleUrls: ['./solution1-paragraph.component.scss']
+})
+export class Solution1ParagraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
